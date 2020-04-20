2376f0858d1fe95872a84a6fff88bdade0b2321d
Satrajit Ghosh <satra@mit.edu>
2020-04-20 17:58:19 -0400

    fix: support schema ui updates

---
+++ ui-changes/src/config.js
@@ -0,0 +1,5 @@
+export default {
+  /* eslint-disable */
+  githubSrc: 'https://raw.githubusercontent.com/ReproNim/reproschema/master/protocols/VoicePilot/VoicePilot_schema',
+  contentSrc: 'https://raw.githubusercontent.com/ReproNim/reproschema/master/protocols/VoicePilot/README.md'
+}
