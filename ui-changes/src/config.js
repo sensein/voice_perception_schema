52ab28f17fe59d2dab27584360e8291910f29411
Sanu Ann Abraham <abraham.sanuann@gmail.com>
2020-11-30 11:06:00 -0500

    test banner message

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   /* eslint-disable */
   githubSrc: 'https://raw.githubusercontent.com/sensein/covid19/master/protocol/Covid19_schema',
-  banner: 'This protocol is under construction.',
+  banner: 'This protocol is under construction. Test',
   startButton: 'Join',
   assetsPublicPath: '/covid19/',
   backendServer: 'https://sig.mit.edu/vb'
