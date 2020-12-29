e64745400d023ac89c3257e6596727ca4059c1a1
Sanu Ann <sanuann@mit.edu>
2020-12-28 23:56:53 -0500

    revert to previous state

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,6 +1,10 @@
 module.exports = {
   /* eslint-disable */
   githubSrc: 'https://raw.githubusercontent.com/sanuann/covid19/master/protocol/Covid19_schema',
+  banner: {
+    "en": 'This protocol is under construction.',
+    "es": "This protocol is under construction_es"
+  },
   startButton: {
     "en": 'Join',
     "es": 'Participar'
