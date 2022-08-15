4e3bb8c467f6362efa167260bc98d6d647c160a9
GasserElbanna <gasser_elbanna@epfl.ch>
2022-08-15 09:06:31 -0400

    :art: add personal info activity

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,15 +1,14 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/sensein/voice_perception_schema/master/protocol/Covid19_schema',
+  githubSrc: 'https://raw.githubusercontent.com/sensein/voice_perception_schema/master/protocol/Voice_perception_schema',
   startButton: {
-    "en": 'Join',
-    "es": 'Participar'
+    "en": 'Join'
   },
   assetsPublicPath: '/voice_perception_schema/',
   backendServer: 'https://sig.mit.edu/vb',
   showHelp: true,
-  contact: 'voice_survey@mit.edu',
-  emailSubject: 'Help with Covid19 study',
-  checkMediaPermission: true
+  // contact: 'voice_survey@mit.edu',
+  // emailSubject: 'Help with Covid19 study',
+  checkMediaPermission: false
 };
 
